5627a041ebc1a1daf71586a3bacea88ed7bc3afa
rodrigomacedo07 <rodrigo.macedo7@gmail.com>
2026-04-02 00:49:17 -0300

    Feat: implementação de analytics no head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,33 @@ export default function RootLayout({
           src="https://cloud.umami.is/script.js" 
           data-website-id="6ab1b84e-6645-4a41-9739-d440514c27f7"
         ></script>
+        {/* Google Analytics 4 */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZWZ0WCB157"
+          strategy="afterInteractive"
+        />
+        <Script id="ga4-script" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            window.gtag = gtag;
+
+            gtag('js', new Date());
+            gtag('config', 'G-ZWZ0WCB157', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+        {/* Microsoft Clarity */}
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "w54z649iyc");
+          `}
+        </Script>
       </head>
       <body>
         {children}
